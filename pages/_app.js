6cfaaa79ffad18600575f4d057643db67b72c8c2
whome1103 <cuf1103@gmail.com>
2026-07-04 16:09:48 +0900

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-5MRDSV85PW"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5MRDSV85PW');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
